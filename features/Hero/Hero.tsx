a543ffb3782c3b9825f3ac03de86d1efbb78f08b
homfarnam <homfarnam@gmail.com>
2020-09-10 14:23:11 +0430

    fix: fix responsive

--- features/Hero/Hero.tsx
+++ features/Hero/Hero.tsx
@@ -12,7 +12,7 @@ const MyTitle = styled.h1`
 const Hero: React.FC<HeroProps> = ({ className = "" }) => {
   return (
     <div className={`flex flex-col justify-center ${className}`}>
-      <MyTitle className="text-6xl flex justify-center pb-8">
+      <MyTitle className="text-3xl md:text-6xl flex justify-center pb-8">
         Farnam Homayounfar
       </MyTitle>
       <h3 className="text-2xl flex justify-center">Frontend developer</h3>
