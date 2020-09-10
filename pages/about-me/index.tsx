d486d853078915566ba0879d0d90b172745cf193
homfarnam <homfarnam@gmail.com>
2020-09-10 14:24:44 +0430

    feat: change responsive

--- pages/about-me/index.tsx
+++ pages/about-me/index.tsx
@@ -3,26 +3,28 @@ import { Title } from "components"
 
 export default function AboutMe() {
   return (
-    <MyLayout title="Farnam Homayounfar">
+    <MyLayout title="About Me">
       <Title className="flex justify-center py-10">About Me</Title>
-      <p>
-        I am Farnam Homayounfar, I'm 23 and I am a Frontend developer for 5
-        years.
-      </p>
-      <p>
-        I am experienced with Html, CSS, Sass, JavaScript, and Reactjs/React
-        native, React-Redux, Next js and, MongoDB. Also, I can program with
-        Python.
-      </p>
-      <p>I have Some knowledge in Backend like Node js and PHP and Django.</p>
-      <p>
-        I am an expert in Wordpress too. I have some skills in UI/UX Design.
-      </p>
-      <p>Also, I can work with Zepline and Adobe XD.</p>
-      <p>
-        In these years I learned many things with self-study, I can learn
-        anything quickly.
-      </p>
+      <div className="w-6/12 mx-auto text-2xl py-5">
+        <p>
+          I am Farnam Homayounfar, I'm 23 and I am a Frontend developer for 5
+          years.
+        </p>
+        <p>
+          I am experienced with Html, CSS, Sass, JavaScript, and Reactjs/React
+          native, React-Redux, Next js and, MongoDB. Also, I can program with
+          Python.
+        </p>
+        <p>I have Some knowledge in Backend like Node js and PHP and Django.</p>
+        <p>
+          I am an expert in Wordpress too. I have some skills in UI/UX Design.
+        </p>
+        <p>Also, I can work with Zepline and Adobe XD.</p>
+        <p>
+          In these years I learned many things with self-study, I can learn
+          anything quickly.
+        </p>
+      </div>
     </MyLayout>
   )
 }
