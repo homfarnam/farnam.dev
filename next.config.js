c34743957080e640b5fc1424ab87f74ade20b386
homfarnam <homfarnam@gmail.com>
2020-09-10 14:23:28 +0430

    feat: change next configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const withCSS = require("@zeit/next-css")
-// module.exports = withCSS({
-//   cssLoaderOptions: {
-//     url: false,
-//   },
-// })
+const withFonts = require("next-fonts")
+
+module.exports = withCSS({})
+
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+
+    // Important: return the modified config
+    return config
+  },
+}
