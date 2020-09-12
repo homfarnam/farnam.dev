be1cbce25af83f3b8ae812ed703f23eac49262b1
homfarnam <homfarnam@gmail.com>
2020-09-12 11:59:00 +0430

    feat: add clip path for footer background

--- features/Footer/Footer.tsx
+++ features/Footer/Footer.tsx
@@ -1,11 +1,18 @@
 import * as React from "react"
+import styled from "styled-components"
+
+const MyFooter = styled.footer`
+  clip-path: polygon(0 0, 100% 25%, 100% 100%, 0% 100%);
+  background-color: #333333;
+  height: 150px;
+`
 
 interface FooterProps {}
 
 const Footer: React.FC<FooterProps> = () => {
   return (
-    <footer className="bg-black flex-shrink-0">
-      <div className="container mx-auto">
+    <MyFooter className="flex-shrink-0">
+      <div className="container mx-auto my-10">
         <div className="flex flex-col lg:flex-row justify-between items-center p-12 lg:p-8 bg-origin-grey text-white ">
           <div className="flex-wrap space-y-8">
             {/* footer menu */}
@@ -27,7 +34,7 @@ const Footer: React.FC<FooterProps> = () => {
           </div>
         </div>
       </div>
-    </footer>
+    </MyFooter>
   )
 }
 
