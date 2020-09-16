2c549038edf8be831d465aaf9a1f5a123149b85d
homfarnam <homfarnam@gmail.com>
2020-09-16 09:19:29 +0430

    feat: get posts from strapi

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,6 @@
 export { default as Title } from "./Title"
 export { default as TextInput } from "./TextInput"
 export { default as Button } from "./Button"
+export { default as Card } from "./Card"
+export { default as Nav } from "./Nav"
+export { default as Articles } from "./Articles"
