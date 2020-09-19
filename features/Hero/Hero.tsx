cdfe8b49258df06cf3e9d314b913d0c61eb253a3
homfarnam <homfarnam@gmail.com>
2020-09-19 19:23:20 +0430

    fix: change h3 to h2

--- features/Hero/Hero.tsx
+++ features/Hero/Hero.tsx
@@ -15,7 +15,7 @@ const Hero: React.FC<HeroProps> = ({ className = "" }) => {
       <MyTitle className="text-3xl md:text-6xl flex justify-center pb-8">
         Farnam Homayounfar
       </MyTitle>
-      <h3 className="text-2xl flex justify-center">Frontend developer</h3>
+      <h2 className="text-2xl flex justify-center">Frontend developer</h2>
     </div>
   )
 }
