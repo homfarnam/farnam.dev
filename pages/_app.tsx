09e2835442d01c823b08412b25f9eda6677278dc
homfarnam <homfarnam@gmail.com>
2020-09-21 12:20:25 +0330

    chore: comment unused font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,10 +12,10 @@ function MyApp({ Component, pageProps, apollo }: any) {
             name="viewport"
             content="initial-scale=1.0, width=device-width"
           />
-          <link
+          {/* <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Staatliches"
-          />
+          /> */}
           {/* <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css"
