a1258f649dd5b640932cd5f6d56329b0ad1b1757
homfarnam <homfarnam@gmail.com>
2021-06-15 22:39:05 +0430

    Change hero section style

--- features/Hero/Hero.tsx
+++ features/Hero/Hero.tsx
@@ -1,4 +1,29 @@
-import { BackText, Bio, MyPic, MyTitle } from "./styles"
+import Button from "@components/Button"
+import Image from "next/image"
+import styled from "styled-components"
+import codehtml from "../../public/codehtml.svg"
+
+const GlassCard = styled.div`
+  position: absolute;
+  width: 320px;
+  height: 448px;
+  left: 975px;
+  top: 234px;
+
+  background: linear-gradient(
+    152.97deg,
+    rgba(255, 255, 255, 0.2) 0%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  backdrop-filter: blur(42px);
+  /* Note: backdrop-filter has minimal browser support */
+
+  border-radius: 16px;
+`
+
+const Code = styled.div`
+  filter: drop-shadow(0px 4px 60px rgba(0, 0, 0, 0.25));
+`
 
 interface HeroProps {
   className?: string
@@ -6,34 +31,60 @@ interface HeroProps {
 
 const Hero: React.FC<HeroProps> = ({ className = "" }) => {
   return (
-    <div
-      className={`flex flex-col lg:flex-row justify-center lg:justify-around items-center ${className}`}
-    >
+    <div className={`flex flex-col lg:flex-row  items-center ${className}`}>
       <div className="flex flex-col items-start">
-        <BackText>Hello.</BackText>
-        <div className="z-10 flex flex-col w-screen lg:w-auto items-start lg:justify-center mx-20 lg:mx-auto lg:px-0">
-          <MyTitle className="text-2xl font-extrabold lg:text-4xl flex w-full lg:w-auto justify-center pb-8">
-            Hi, I’m Farnam Homayounfar.
-          </MyTitle>
-          <Bio className="flex flex-col justify-center ml-5 lg:ml-auto px-3 lg:px-0">
-            <p className="text-Hero-subtitle text-3xl mb-3">Who I am?</p>
-            <div className="mt-2">
-              <p>
-                I’m a freelance Frontend Developer. I develop systems to help my
-                clients be more independant and grow their bussiness easily.
+        <Image
+          src="/sphere.svg"
+          layout="fixed"
+          width={186}
+          height={186}
+          objectFit="cover"
+        />
+        <div className="z-10 flex flex-row w-screen lg:w-auto items-start justify-start ">
+          <div className="flex flex-row justify-between ml-5 px-3 w-full ">
+            <div className="mt-2 flex-col text-white text-4xl w-1/2 space-y-5">
+              <p className="leading-10 font-normal font-pilat">
+                Well, it it new era and for success in it, you need new era’s
+                developer. thats who am i.
               </p>
-              <p>
-                So far, I helped Legaler, Canymess, Hawei Center, Paklean and
-                many more.
+              <p className="font-thin font-apparel">
+                Feel Free To get in touch and see my work
               </p>
-              <p>You can find my work on Github.</p>
+              <Button variant="black">See more </Button>
             </div>
-          </Bio>
+            <div className="z-0">
+              <Image
+                src="/Sphere_gold.svg"
+                className="z-30"
+                layout="fixed"
+                width={338}
+                height={338}
+                objectFit="cover"
+              />
+            </div>
+            <GlassCard className="z-20  ml-[200px] -mt-32">
+              <Code className="z-50 relative right-52 -top-28">
+                <Image
+                  src={codehtml}
+                  className="z-30"
+                  layout="fixed"
+                  width={720}
+                  height={720}
+                  objectFit="cover"
+                />
+              </Code>
+            </GlassCard>
+            <div className="relative -top-56">
+              <Image
+                src="/Color-Black-Matte.svg"
+                layout="fixed"
+                width={186}
+                height={186}
+                objectFit="cover"
+              />
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="flex flex-row justify-center w-full lg:w-auto">
-        <MyPic src="/violin.jpeg" alt="violin" />
       </div>
     </div>
   )
