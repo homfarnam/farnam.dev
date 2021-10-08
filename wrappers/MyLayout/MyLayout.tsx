6d97bbccf27f40d2b65c4dd9e48fdbb0b5213f21
homfarnam <homfarnam@gmail.com>
2021-10-08 20:02:29 +0330

    Change page title

--- wrappers/MyLayout/MyLayout.tsx
+++ wrappers/MyLayout/MyLayout.tsx
@@ -35,7 +35,7 @@ const MyLayout: React.FC<MyLayoutProps> = ({ children, title }) => {
           as="font"
           crossOrigin=""
         />
-        <title key="page-title">{`Farnam Homayounfar - ${title}`}</title>
+        <title key="page-title">{`Farnam Homayounfard - ${title}`}</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
