c3ea1be09f32abb2687ab4ed0cbad490d6112ccf
homfarnam <homfarnam@gmail.com>
2022-01-08 00:58:24 +0330

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Hero = dynamic(() => import("../features/Hero/Hero"))
 
 export default function Home() {
   return (
-    <MyLayout title="Homepage">
+    <MyLayout title="Homepage" className="container flex-grow mx-auto">
       <Hero className="pt-16" />
       <MyInfo />
 
