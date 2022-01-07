a79136b75916e0c2f59222976806405c90bc41ea
homfarnam <homfarnam@gmail.com>
2022-01-07 08:17:23 +0330

    Import glass component

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,15 +1,30 @@
 import * as React from "react"
 import { Title } from "components"
 import { MyLayout } from "wrappers"
+import Round from "../../public/RoundCube-White-Matte.svg"
+import Image from "next/image"
+import Glasscard from "@components/Glasscard/Glasscard"
 
 interface Blog {}
 
 const Blog: React.FC<Blog> = () => {
   return (
     <MyLayout>
-      <div>
-        <Title className="flex justify-center pt-5">Blog</Title>
-        <div>sdsd</div>
+      <div className="container">
+        <div className="">
+          <Image
+            src="/sphere.svg"
+            layout="fixed"
+            width={186}
+            height={186}
+            objectFit="cover"
+            alt="sphere"
+          />
+        </div>
+        <Title className="text-5xl text-white">The Blog </Title>
+        <div className="w-full">
+          <Glasscard />
+        </div>
       </div>
     </MyLayout>
   )
