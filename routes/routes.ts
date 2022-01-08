e095ce1fb2e13707d188ced5ec3c19b36364768e
homfarnam <homfarnam@gmail.com>
2022-01-08 20:38:47 +0330

    Add routes folder

---
+++ routes/routes.ts
@@ -0,0 +1,26 @@
+export interface RouteType {
+  href: string
+  text: string
+  color?: string
+  withParams?: (param: string | undefined) => string
+}
+
+export const routes = {
+  root: { href: "/", text: "Root" },
+  aboutMe: {
+    href: "/about-me",
+    text: "About me",
+  },
+  contactMe: {
+    href: "/contact-me",
+    text: "Contact Me",
+  },
+  blog: {
+    href: "/blog",
+    text: "blog",
+  },
+  resume: {
+    href: "/resume",
+    text: "resume",
+  },
+}
