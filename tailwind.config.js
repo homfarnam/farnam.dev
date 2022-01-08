121faeff5fac8da2db85fd71834c61a171d77c7d
homfarnam <homfarnam@gmail.com>
2022-01-08 20:39:08 +0330

    Add root files

---
+++ tailwind.config.js
@@ -0,0 +1,76 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  purge: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./features/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./wrappers/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  theme: {
+    fontFamily: {
+      custom: ["Titillium Web", "sans-serif"],
+      apparel: ["Apparel", "sans-serif"],
+      pilat: ["Pilat Extended", "sans-serif"],
+      Montserrat_Alternate: ["Montserrat Alternates", "sans-serif"],
+      sans: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: ["Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
+      mono: [
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        '"Liberation Mono"',
+        '"Courier New"',
+        "monospace",
+      ],
+    },
+    screens: {
+      mobile: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1800px",
+    },
+    scale: {
+      0: "0",
+      25: ".25",
+      50: ".5",
+      65: ".65",
+      75: ".75",
+      85: ".85",
+      90: ".9",
+      95: ".95",
+      100: "1",
+      105: "1.05",
+      110: "1.1",
+      125: "1.25",
+      150: "1.5",
+      200: "2",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  corePlugins: {
+    backgroundImage: true,
+  },
+  plugins: [],
+}
