cdb19762879f6e27f301d843fcc1d100c0a01c4a
homfarnam <homfarnam@gmail.com>
2022-01-08 21:58:44 +0330

    Add new security configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = []
+
 module.exports = {
   reactStrictMode: true,
+}
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+      {
+        key: "X-DNS-Prefetch-Control",
+        value: "on",
+      },
+      {
+        key: "X-XSS-Protection",
+        value: "1; mode=block",
+      },
+    ]
+  },
 }
