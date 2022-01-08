8d0c95b533a8f00da52f4bf5756d1e9a443b36f0
homfarnam <homfarnam@gmail.com>
2022-01-08 22:04:17 +0330

    Change security configs

--- next.config.js
+++ next.config.js
@@ -4,14 +4,7 @@ const securityHeaders = [
     key: "X-DNS-Prefetch-Control",
     value: "on",
   },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload",
-  },
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
-  },
+
   {
     key: "X-Frame-Options",
     value: "SAMEORIGIN",
@@ -19,14 +12,6 @@ const securityHeaders = [
   {
     key: "Permissions-Policy",
     value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
   },
 ]
 
