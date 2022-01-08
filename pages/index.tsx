b0fe845a1e6ce3cd8febbf828e7ef1ba067f2841
homfarnam <homfarnam@gmail.com>
2022-01-08 21:38:08 +0330

    Add dynamic import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import dynamic from "next/dynamic"
-import { MyInfo } from "../components"
-import TechCards from "../components/HomeComponents/Cards"
-import ContactUs from "../components/HomeComponents/Contact-us"
 import { MyLayout } from "../wrappers"
 
 const Hero = dynamic(() => import("../features/Hero/Hero"))
+const MyInfo = dynamic(() => import("../components/MyInfo/MyInfo"))
+const TechCards = dynamic(() => import("../components/HomeComponents/Cards"))
+const ContactUs = dynamic(
+  () => import("../components/HomeComponents/Contact-us")
+)
 
 export default function Home() {
   return (
