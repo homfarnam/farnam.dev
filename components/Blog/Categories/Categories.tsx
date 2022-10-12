af246f6200da4eb5956135ec0e7d95cef58d141e
homfarnam <homfarnam@gmail.com>
2022-10-13 00:34:11 +0200

    Fix render items

--- components/Blog/Categories/Categories.tsx
+++ components/Blog/Categories/Categories.tsx
@@ -1,10 +1,10 @@
 import ScrollContainer from "react-indiana-drag-scroll"
 import Image from "next/image"
 import { Button, Glasscard } from "components"
-import { Articles_articles } from "graphql/Queries/__generated__/Articles"
+import { Articles_articles_data } from "graphql/Queries/__generated__/Articles"
 
 interface CategoriesProps {
-  data: Articles_articles[]
+  data: Articles_articles_data[]
   selectedPost: (id: string | number) => void
 }
 
@@ -14,21 +14,26 @@ const Categories: React.FC<CategoriesProps> = ({ data, selectedPost }) => {
       <div className="blog__cards--categories">
         {data.map((item) => {
           const myLoader = () => {
-            return `https://farnamh.ir${item.image?.url}`
+            return `https://farnam.tech${item.attributes?.Coverphoto?.data?.attributes?.url}`
           }
           return (
             <div key={item.id} className="blog__cards--categories__card">
               <Glasscard className="z-0" />
               <Glasscard className="relative p-5 -top-32 -right-10">
-                {item.image && (
+                {item.attributes?.Coverphoto?.data?.attributes?.url && (
                   <Image
                     className="rounded-3xl"
-                    src={item.image?.url as any}
-                    alt={item.image?.alternativeText as any}
+                    src={
+                      item.attributes?.Coverphoto?.data?.attributes?.url as any
+                    }
+                    alt={
+                      item.attributes?.Coverphoto?.data?.attributes
+                        ?.alternativeText as any
+                    }
                     layout="fill"
                     loader={myLoader}
                     onClick={() => {
-                      selectedPost(item.id)
+                      selectedPost(item.id as string)
                     }}
                   />
                 )}
@@ -36,15 +41,15 @@ const Categories: React.FC<CategoriesProps> = ({ data, selectedPost }) => {
               <div
                 className="blog__cards--categories__card__data"
                 onClick={() => {
-                  selectedPost(item.id)
+                  selectedPost(item.id as string)
                 }}
               >
-                <h3>{item.title}</h3>
+                <h3>{item.attributes?.title}</h3>
                 <Button
                   roundFull
                   className="bg-[#FFB703] font-Montserrat_Alternate font-bold rounded-lg"
                   onClick={() => {
-                    selectedPost(item.id)
+                    selectedPost(item.id as string)
                   }}
                 >
                   Read more
