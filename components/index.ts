e4857d6a162d7a9fdeb879ce06dbd9452fe06d3c
homfarnam <homfarnam@gmail.com>
2022-10-13 00:34:20 +0200

    Remove unused imports

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,6 @@
 export { default as Title } from "./Title"
 export { default as TextInput } from "./TextInput"
 export { default as Button } from "./Button"
-export { default as Card } from "./Card"
-export { default as Nav } from "./Nav"
-export { default as Articles } from "./Articles"
+
 export { default as MyInfo } from "./MyInfo/MyInfo"
 export { default as Glasscard } from "./Glasscard/Glasscard"
