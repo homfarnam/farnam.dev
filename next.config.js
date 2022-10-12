0a86173738a8e8123ae1a11a3bac302f9183a8b0
homfarnam <homfarnam@gmail.com>
2022-10-13 00:34:26 +0200

    Change next js config file

--- next.config.js
+++ next.config.js
@@ -1,43 +1,35 @@
-const withCSS = require("@zeit/next-css")
+// const withCSS = require("@zeit/next-css")
 const withPurgeCss = require("next-purgecss")
 const withFonts = require("next-fonts")
 const withBabelMinify = require("next-babel-minify")({
   comments: false,
 })
+const withPlugins = require("next-compose-plugins")
 
-module.exports = withBabelMinify({
-  webpack(config, options) {
-    return config
-  },
-})
+module.exports = {
+  reactStrictMode: true,
+}
 
 module.exports = {
   images: {
-    domains: ["nextjs.org", "farnamh.ir"],
+    domains: ["nextjs.org", "farnamh.ir", "farnam.tech"],
   },
 }
 
-module.exports = withCSS(
-  {
-    env: {
-      API_URL: process.env.API_URL,
-    },
-  },
-  withPurgeCss({
-    purgeCssPaths: [
-      "pages/**/*",
-      "components/**/*", // also scan other-components folder
-    ],
-  })
-)
+// module.exports = withPlugins([], {
+//   webpack5: false,
+// })
 
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
-
-    // Important: return the modified config
-    return config
-  },
-}
+// module.exports = withCSS(
+//   {
+//     env: {
+//       API_URL: process.env.API_URL,
+//     },
+//   },
+//   withPurgeCss({
+//     purgeCssPaths: [
+//       "pages/**/*",
+//       "components/**/*", // also scan other-components folder
+//     ],
+//   })
+// )
