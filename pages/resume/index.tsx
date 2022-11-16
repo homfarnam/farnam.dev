426f1f176ddcb1a7a8bb50c93b66b26faf8c2ff8
homfarnam <homfarnam@gmail.com>
2022-11-16 23:51:17 +0100

    Add link to email and cv

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -5,6 +5,7 @@ import { Articles_articles } from "../../graphql/Queries/__generated__/Articles"
 // import styled from "styled-components"
 import FHImage from "../../public/fh.png"
 import useWindowSize from "../../hooks/useWindowSize"
+import Link from "next/link"
 
 interface Blog {
   data: Articles_articles[]
@@ -39,16 +40,23 @@ const Resume: React.FC<Blog> = () => {
               </p>
             </h3>
             <span className="">Frontend developer</span>
-            <button className="cvButton2">Hire me</button>
+            <Link href="mailto:homfarnam@gmail.com" passHref>
+              <button className="cvButton2">Hire me</button>
+            </Link>
           </div>
         </div>
         <div className="resume__main--about">
-          <span className="">Based in Iran</span>
+          <span className="">Based in The Netherlands</span>
           <p>
-            congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus
-            unum. Defacto lingo est igpay atinlay. Marquee selectus
+            Since March 2022, I migrate to the Netherlands to work as a frontend
+            developer.
           </p>
-          <button className="cvButton2">Download CV</button>
+          <Link
+            href="https://farnam.tech/uploads/Farnam_Homayounfard_Resume_a23cb7c5e9.pdf?updated_at=2022-11-16T22:48:29.282Z"
+            passHref
+          >
+            <button className="cvButton2">Download CV</button>
+          </Link>
         </div>
       </div>
     </MyLayout>
